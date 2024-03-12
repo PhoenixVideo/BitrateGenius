9c10d146701d0fe64e94fe2ad14772b88970a2c4
AmrithaPrem <46924276+AmrithaPrem@users.noreply.github.com>
2024-03-12 19:24:43 +0100

    integrating prediction model to code

--- main.py
+++ main.py
@@ -3,63 +3,77 @@ import os
 import pickle
 import pandas as pd
 import time
-def run_cmake_build(build_dir):
-    # Change directory to the build folder
+import platform
+import math
+import argparse
+import sys
+
+
+def run_executable_in_build(system_platform, build_dir, parameters):
     os.chdir(build_dir)
+    executable_path = None
+    if system_platform == 'Windows':
+        executable_path = os.path.join(build_dir, "vca_release.exe")
+        try:
+            subprocess.run([executable_path] + parameters, check=True)
+            print("Executable ran successfully!")
+        except subprocess.CalledProcessError:
+            print("Error: Failed to execute the executable.")
+    elif system_platform == 'Linux':
+        dist = platform.dist()
+        if dist[0].lower() == 'ubuntu':
+            executable_path = os.path.join(build_dir, "vca_release")
+        try:
+            subprocess.run([executable_path] + parameters, check=True)
+            print("Executable ran successfully!")
+        except subprocess.CalledProcessError:
+            print("Error: Failed to execute the executable.")
+    else:
+        print("BitrateGenius currently works only in Windows or Ubuntu")
 
-    # Configure the build with CMake
-    cmake_configure_cmd = ["cmake", "-DCMAKE_BUILD_TYPE=Release", "../VCA/"]
-    subprocess.run(cmake_configure_cmd, check=True)
-
-    # Build the project
-    cmake_build_cmd = ["cmake", "--build", "."]
-    try:
-        subprocess.run(cmake_build_cmd, check=True)
-        print("Build completed successfully!")
-    except subprocess.CalledProcessError:
-        print("Build failed!")
-
-def run_executable_in_build(build_dir,parameters):
-    # Change directory to the build folder
-    os.chdir(build_dir)
-
-    # Construct the path to the executable
-    executable_path = os.path.join(build_dir, "vca.exe")
-
-    # Execute the executable with the provided parameters
-    try:
-        subprocess.run([executable_path] + parameters, check=True)
-        print("Executable ran successfully!")
-    except subprocess.CalledProcessError:
-        print("Error: Failed to execute the executable.")
 
 def predict_bitrate(parameters_vector):
-    # Load the pickled model
-    loaded_model = pickle.load(open('C:\\Users\\amrit\\Desktop\\Video streaming papers\\ICIP Challenge\\best_br_model.pkl', 'rb'))
-
-    # Assuming you have new data X_new for prediction
-    y_pred_new = loaded_model.predict(parameters_vector)
-
-    return y_pred_new
+    bitrate_pred_model = pickle.load(open(os.path.join(build_directory, 'bitrate_predictor.pkl'), 'rb'))
+    pred_bitrate = bitrate_pred_model.predict(parameters_vector)
+    return pred_bitrate
 
 
-# Example usage:
+def clean_up():
+    file_path = os.path.dirname(os.path.abspath(__file__)) + "/build/temp.csv"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
 if __name__ == "__main__":
 
-    #build_directory = "build/"
-    #run_cmake_build(build_directory)
-    build_directory = "C:\\Users\\amrit\\Documents\\GitHub\\BitrateGenius\\build\\source\\apps\\vca\\Debug"
+    # input arguments
+    parser = argparse.ArgumentParser(
+        description="BitrateGenius: Predict bitrate of x264 CRF26 2160p 60fps encoding (medium preset).")
+    parser.add_argument("--inputvideo", help="Input file path",
+                        default="C:\\Users\\amrit\\Desktop\\Video_streaming_papers\\0001.y4m")
+    args = parser.parse_args()
+    input_video_file = args.inputvideo
 
-    parameters = ["--input", "C:\\Users\\amrit\\Downloads\\elephants_dream_360p24.y4m\\elephants_dream_360p24.y4m", "--complexity-csv",
-                  "C:\\Users\\amrit\\Documents\\GitHub\\BitrateGenius\\build\\temp.csv", "--frames",  "100","--no-edgedensity" ]
+    sys_platform = platform.system()
 
-    start_time_exe = time.time()
-    run_executable_in_build(build_directory, parameters)
-    end_time_exe = time.time()
-    execution_time =  end_time_exe - start_time_exe
-    print("Execution time exe:", execution_time, "seconds")
-    start_time_csv = time.time()
-    df = pd.read_csv("C:\\Users\\amrit\\Documents\\GitHub\\BitrateGenius\\build\\temp.csv")
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    build_directory = current_directory + "/build/"
+
+    video_file_extension = os.path.splitext(input_video_file)[1]
+    if video_file_extension == '.y4m':
+        vca_parameters = ["--input", input_video_file, "--complexity-csv", current_directory + "/build/temp.csv", "--no-edgedensity", "--no-dctenergy-chroma"]
+    elif video_file_extension == '.yuv':
+        vca_parameters = ["--input", input_video_file, "--input-res", "3840x2160", "--fps", "60", "--complexity-csv",
+                          current_directory + "/build/temp.csv", "--no-edgedensity", "--no-dctenergy-chroma"]
+    else:
+        print("Video extension other than .y4m and .yuv is not supported")
+        sys.exit()
+
+    start_time = time.time()
+
+    run_executable_in_build(sys_platform, build_directory, vca_parameters)
+
+    df = pd.read_csv(current_directory + "/build/temp.csv")
     E_max = df["E"].max()
     E_std = df["E"].std()
     h_mean = df["h"].mean()
@@ -69,29 +83,17 @@ if __name__ == "__main__":
     entropyDiff_mean = df["entropyDiff"].mean()
     entropyU_max = df["entropyU"].max()
     entropyV_max = df["entropyV"].max()
-    parameters_vector = [[E_max, E_std,h_mean, h_std,L_mean,entropy_max,entropyDiff_mean,entropyU_max,entropyV_max]]
-    print(parameters_vector)
+    model_parameters = [
+        [E_max, E_std, h_mean, h_std, L_mean, entropy_max, entropyDiff_mean, entropyU_max, entropyV_max]]
 
-    y_pred_new = predict_bitrate(parameters_vector)
+    predicted_bitrate = predict_bitrate(model_parameters)
 
-    end_time_csv = time.time()
-    execution_time_csv = end_time_csv - start_time_csv
+    end_time = time.time()
 
-    total_execution_time = execution_time + execution_time_csv
+    total_execution_time = end_time - start_time
+
     print("total_execution_time", total_execution_time, "seconds")
 
+    print("The predicted bitrate (in kbps) is: " + str(math.exp(predicted_bitrate[0])) + " kbps")
 
-    # Print or use the predictions
-    print(y_pred_new)
-
-    print("All Done")
-
-
-
-
-
-
-
-
-
-
+    clean_up()
